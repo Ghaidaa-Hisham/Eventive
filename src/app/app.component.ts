6a5d7b92160f4fd102344ee9b194fea2a0a203df
Ghaidaa Hisham <ghedaa22nemr@gmail.com>
2024-09-22 01:54:37 +0300

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./components/footer/footer.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DestinationComponent } from './components/destination/destination.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent , NavbarComponent, DestinationComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
