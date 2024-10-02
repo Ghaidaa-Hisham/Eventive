b3814f07d9651279abd9a721401415d6c771f756
Zeina Wady <zeinawady@gmail.com>
2024-10-02 18:46:21 +0300

    adding capacity , likes , events , location components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,16 @@ import { LoginComponent } from "./components/login/login.component";
 import { SearchEventComponent } from "./components/search-event/search-event.component";
 import { TicketsComponent } from "./components/tickets/tickets.component";
 import { TrendsComponent } from "./components/trends/trends.component";
+import { LikeComponent } from "./components/like/like.component";
+import { EventsComponent } from "./components/events/events.component";
+import { LocationComponent } from "./components/location/location.component";
+import { CapacityComponent } from "./components/capacity/capacity.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, NavbarComponent, DestinationComponent, LoginComponent, SearchEventComponent, TicketsComponent, TrendsComponent],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent, DestinationComponent, LoginComponent, SearchEventComponent, TicketsComponent, TrendsComponent, LikeComponent, EventsComponent, LocationComponent, CapacityComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
