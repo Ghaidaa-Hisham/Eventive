c844e5c15678663025cc8ec87bba87f64ae5615a
Zeina Wady <zeinawady@gmail.com>
2024-10-03 04:04:01 +0300

    adjusting home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,15 @@ import { LikeComponent } from "./components/like/like.component";
 import { EventsComponent } from "./components/events/events.component";
 import { LocationComponent } from "./components/location/location.component";
 import { CapacityComponent } from "./components/capacity/capacity.component";
+import { SignUpComponent } from "./components/sign-up/sign-up.component";
+import { PopularCitiesComponent } from "./components/popular-cities/popular-cities.component";
+import { CarouselComponent } from "./components/carousel/carousel.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, NavbarComponent, DestinationComponent, LoginComponent, SearchEventComponent, TicketsComponent, TrendsComponent, LikeComponent, EventsComponent, LocationComponent, CapacityComponent],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent, DestinationComponent, LoginComponent, SearchEventComponent, TicketsComponent, TrendsComponent, LikeComponent, EventsComponent, LocationComponent, CapacityComponent, SignUpComponent, PopularCitiesComponent, CarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
