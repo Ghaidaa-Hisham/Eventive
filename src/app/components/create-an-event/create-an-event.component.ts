34ea40f6462adcbf6e408dbf4522f56d9de2a75d
Ghaidaa Hisham <ghedaa22nemr@gmail.com>
2024-10-04 22:38:08 +0300

    upload the destination and create event

---
+++ src/app/components/create-an-event/create-an-event.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-an-event',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-an-event.component.html',
+  styleUrl: './create-an-event.component.scss'
+})
+export class CreateAnEventComponent {
+
+}
